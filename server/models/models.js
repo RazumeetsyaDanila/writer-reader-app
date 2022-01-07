4e39b4d9bf6b2f1c3d0b099c4551ff8aed970556
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-01-08 00:35:33 +0300

    db init

---
+++ server/models/models.js
@@ -0,0 +1,26 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    user_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    login: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'}
+})
+
+const Message = sequelize.define('message', {
+    message_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    message_date: {type: DataTypes.DATE},
+    message_text: {type: DataTypes.STRING}
+})
+
+const UserMessage = sequelize.define('user_message', {id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}})
+
+User.belongsToMany(Message, {through: UserMessage})
+Message.belongsToMany(User, {through: UserMessage})
+
+module.exports = {
+    User,
+    Message,
+    UserMessage
+}
