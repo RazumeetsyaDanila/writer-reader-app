4677ff14a3732da117a71d056b44396f8a7497f1
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-01-09 19:59:58 +0300

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ANTI CODING CODING CLUB!
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
